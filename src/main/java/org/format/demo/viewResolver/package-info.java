385773cba0b35df4bdc070b80547cf7ffc45e976
cx_yang <84593cxy>
2017-08-29 18:41:54 +0800

    注释

---
+++ src/main/java/org/format/demo/viewResolver/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Function: 自定义视图解析器
+ * Reason: 一般可以通过继承 UrlBasedViewResolver 并重写 buildView 方法实现
+ * Date: 2017/8/29 15:23 </br>
+ *
+ * @author: cx.yang
+ * @since: Thinkingbar Web Project 1.0
+ */
+package org.format.demo.viewResolver;
