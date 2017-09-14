9be3ef819f04eef45dffbd8853db502f9b7779fc
yangcx <yangcx@thinkingbar.cn>
2017-09-14 16:56:16 +0800

    二叉树遍历

--- src/test/java/test/binaryTree/BinaryTreeUtils.java
+++ src/test/java/test/binaryTree/BinaryTreeUtils.java
@@ -10,49 +10,157 @@ package test.binaryTree;
  */
 public class BinaryTreeUtils {
 
+    //二叉树递归遍历
+
+    /**
+     * 后序
+     *
+     * @param node
+     */
+    public void postOrder(Node node) {
+        if (node == null) {
+            return;
+        } else {
+            postOrder(node.getLeftChild());
+            postOrder(node.getRightChild());
+            System.out.print(node.getData() + " ");
+        }
+    }
+
+    /**
+     * 中序
+     *
+     * @param node
+     */
+    public static void inOrder(Node node) {
+        if (null == node) {
+            return;
+        } else {
+            inOrder(node.getLeftChild());
+            System.out.println(node.getData() + " ");
+            inOrder(node.getRightChild());
+        }
+    }
+
+    /**
+     * 先序
+     *
+     * @param node
+     */
+    public static void preOrder(Node node) {
+        //递归实现
+        if (null == node) {
+            return;
+        } else {
+            System.err.println(node.getData() + " ");
+            preOrder(node.getLeftChild());
+            preOrder(node.getRightChild());
+        }
+        //非递归实现
+        //TODO 利用一个栈，先序遍历即为根先入栈，然后出栈，凡是出栈的元素都打印值
+        Node[] nodes = new Node[3];
+        Node tmp;
+        int level = -1;
+        if (null != node) {
+            level++;
+            nodes[level] = node;
+            while (level > -1) {
+                tmp = nodes[level];
+                level--;
+                System.out.println(tmp.getData() + " ");
+                if (null != tmp.getRightChild()) {
+                    level++;
+                    nodes[level] = tmp.getRightChild();
+                }
+                if (null != tmp.getLeftChild()) {
+                    level++;
+                    nodes[level] = tmp.getLeftChild();
+                }
+            }
+        }
+    }
+
     /**
      * @param exp A(B(D(,G)),C(E,F))
      * @return
      */
-    public static Node createTree(String exp) {
+    public static Node createTreeWhileFor(String exp) {
+        Node result = null;
+        Node tmp = null;
         Node[] nodes = new Node[3];
-        Node b, p = null;
-        int level = -1, k = 0, j = 0;
         char[] exps = exp.toCharArray();
-        char data = exps[j];
-        b = null;
-        while (j < exps.length - 1) {
-            switch (data) {
+        int level = -1;//二叉树层级
+        int direction = 0;//左叉（1）或右叉（2）
+        for (int i = 0; i < exps.length; i++) {
+            char c = exps[i];
+            switch (c) {
                 case '(':
                     level++;
-                    nodes[level] = p;
-                    k = 1;
+                    direction = 1;//左叉
+                    nodes[level] = tmp;
                     break;
                 case ')':
                     level--;
                     break;
                 case ',':
-                    k = 2;
+                    direction = 2;//右叉
                     break;
                 default:
-                    p = new Node(data, null, null);
-                    if (null == b) {
-                        b = p;
+                    //字母
+                    tmp = new Node(c, null, null);
+                    if (direction == 0) {
+                        result = tmp;
+                    } else if (direction == 1) {
+                        nodes[level].setLeftChild(tmp);
+                    } else if (direction == 2) {
+                        nodes[level].setRightChild(tmp);
+                    }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param exp A(B(D(,G)),C(E,F))
+     * @return
+     */
+    public static Node createTreeWithWhile(String exp) {
+        Node[] nodes = new Node[3];
+        Node result, tmp = null;
+        int level = -1, direction = 0;
+        char[] exps = exp.toCharArray();
+        result = null;
+        for (int i = 0; i < exps.length; i++) {
+            char data = exps[i];
+            switch (data) {
+                case '(':
+                    level++;
+                    nodes[level] = tmp;
+                    direction = 1;
+                    break;
+                case ')':
+                    level--;
+                    break;
+                case ',':
+                    direction = 2;
+                    break;
+                default:
+                    tmp = new Node(data, null, null);
+                    if (null == result) {
+                        result = tmp;
                     } else {
-                        switch (k) {
+                        switch (direction) {
                             case 1:
-                                nodes[level].setLeftChild(p);
+                                nodes[level].setLeftChild(tmp);
                                 break;
                             case 2:
-                                nodes[level].setRightChild(p);
+                                nodes[level].setRightChild(tmp);
                                 break;
                         }
                     }
             }
-            j++;
-            data = exps[j];
         }
-        return b;
+        return result;
     }
 
 }
--- src/test/java/test/binaryTree/Test.java
+++ src/test/java/test/binaryTree/Test.java
@@ -10,10 +10,18 @@ package test.binaryTree;
  */
 public class Test {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String exp = "A(B(D(,G)),C(E,F))";
-        Node tree = BinaryTreeUtils.createTree(exp);
-        System.out.println(tree);
+        Node tree = BinaryTreeUtils.createTreeWithWhile(exp);
+        System.out.println("BinaryTree 创建：" + tree);
+        Node result = BinaryTreeUtils.createTreeWhileFor(exp);
+        System.out.println("BinaryTree 创建：" + result);
+
+        //先序打印
+        BinaryTreeUtils.preOrder(result);
+        //中序打印
+        BinaryTreeUtils.inOrder(result);
+
     }
 
 }
