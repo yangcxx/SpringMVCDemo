2a32dbd6256b9d03d173c54cea1aa5d9d19515a3
cx_yang <84593cxy>
2017-09-14 10:04:47 +0800

    队列模拟

--- src/test/java/test/Queue.java
+++ src/test/java/test/Queue.java
@@ -1,8 +1,10 @@
 package test;
 
+import java.util.Arrays;
+
 /**
- * Function: TODO
- * Reason: TODO ADD REASON(可选).</br>
+ * Function: 队列模拟 - FIFO
+ * Reason: 队列只能在表头进行删除，在表尾进行增加 - 使用于排队系统</br>
  * Date: 2017/9/13 18:54 </br>
  *
  * @author: cx.yang
@@ -51,6 +53,11 @@ public class Queue<T> {
         elementData[rear++] = element;
     }
 
+    /**
+     * FIFO - 先进先出
+     *
+     * @return
+     */
     public T remove() {
         if (isEmpty()) {
             throw new IndexOutOfBoundsException("the queue is empty");
@@ -62,5 +69,47 @@ public class Queue<T> {
 
     public boolean isEmpty() {
         return front == rear;
+    }
+
+    public void clear() {
+        Arrays.fill(elementData, null);
+        front = 0;
+        rear = 0;
+    }
+
+    /**
+     * 首元素
+     *
+     * @return
+     */
+    public T element() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("queue is empty");
+        }
+        return (T) elementData[front];
+    }
+
+    @Override
+    public String toString() {
+        return "Queue{" +
+                "DEFAULT_SIZE=" + DEFAULT_SIZE +
+                ", capacity=" + capacity +
+                ", elementData=" + Arrays.toString(elementData) +
+                ", front=" + front +
+                ", rear=" + rear +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Queue<String> queue = new Queue<String>("ABC", 20);
+        queue.add("DEF");
+        queue.add("egg");
+        queue.remove();
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.size());
+        System.out.println(queue.element());
+        queue.clear();
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.size());
     }
 }
